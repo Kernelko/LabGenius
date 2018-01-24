d8bcf417ba9fa99fe49800f3700c24aca52c3679
Kernelko <zuzanna.onderko@gmail.com>
2018-01-24 18:30:14 +0100

    dataset splitted to 25 % of test set

--- task/main.py
+++ task/main.py
@@ -2,6 +2,11 @@
 
 import pandas as pd
 from string import ascii_letters
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+
 
 def reading():
     """read file into dataframe"""
@@ -27,6 +32,17 @@ x_train_mapped =[]
 for sequence in x_train:
     x_train_mapped.append(map_protein_to_numbers(sequence))
 
+##ALL READ SO NOW SPLITTING THE DATA
 
+X_train, X_test, y_train, y_test = train_test_split(x_train_mapped, y_train, test_size=0.33, random_state=42)
+print(len(X_train), len(X_test))
+
+
+
+"""
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(x_train_mapped, y_train)
+print(clf.feature_importances_)
+"""
 
 
