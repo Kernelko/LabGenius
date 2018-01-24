968a73aa7f3f0603c7723b303fa50500ca53bf78
Kernelko <zuzanna.onderko@gmail.com>
2018-01-24 18:12:40 +0100

    mapping protein to numbers works

--- task/main.py
+++ task/main.py
@@ -3,8 +3,8 @@
 import pandas as pd
 from string import ascii_letters
 
-def training():
-    "training function"
+def reading():
+    """read file into dataframe"""
 
     df = pd.read_csv("TRAINING_SET.csv", names = ["sequence", "score"])
      #read into dataframe
@@ -12,7 +12,7 @@ def training():
     y_train = df["score"][:]
     return x_train, y_train
 
-def mapping(protein):
+def map_protein_to_numbers(protein):
     letters = list(ascii_letters)
     numbers = list(range(len(ascii_letters)))
     map_dict = dict(zip(letters, numbers))
@@ -21,6 +21,8 @@ def mapping(protein):
         result.append(map_dict[letter.lower()])
     return result
 
-x_train, y_train = training()
-print(x_train[1])
-#mapping(cos)
+x_train, y_train = reading()
+
+result = map_protein_to_numbers(x_train[1])
+print(result)
+
