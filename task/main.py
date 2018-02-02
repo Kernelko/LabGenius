1de3beecc02e7e86299df5a137191e8f8a02b9b2
Kernelko <zuzanna.onderko@gmail.com>
2018-02-02 12:25:53 +0100

    almost works, somethings wrong with predicting

--- task/main.py
+++ task/main.py
@@ -11,9 +11,8 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 
 
-
 model = Sequential()
-model.add(Dense(64, input_dim=10, activation='relu'))
+model.add(Dense(120, input_dim=10, activation='sigmoid'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
@@ -22,7 +21,6 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-
 
 def reading():
     """read file into dataframe"""
@@ -37,22 +35,17 @@ x_train, y_train = reading()
 
 """
 # Features are how many aliphatic, aromatic and acidic and basic are in sequence
-
 def feature_selector(protein):
     extract features from protein sequence: counts how many of each amino acid type protein has.
     returns an array with number of amino acids in protein from specific group in a form [Aliphatic, Aromatic, Acidic, Basic, Other]
-
     grouped = [("a", "g", "i","l", "p", "v"),("f", "w", "y"),("d", "e"),("r", "h", "k"), ("n", "c", "q", "m", "s", "t")]
     features_array = [0,0,0,0,0]
-
     for index in range(5):
         for letter in list(protein.lower()):
             if letter in grouped[index]:
                 features_array[index] += 1
     return features_array
-
 x_train_groupped = []
-
 for sequence in x_train:
     x_train_groupped.append(feature_selector(sequence))
 """
@@ -71,16 +64,10 @@ x_train_mapped = []
 for sequence in x_train:
     x_train_mapped.append(map_protein_to_numbers(sequence))
 
-
-
-
 ##SPLITTING THE DATA
 
 X_train, X_test, y_train, y_test = train_test_split(x_train_mapped, y_train, test_size=0.33, random_state=42)
 print(len(X_train), len(X_test))
-
-##Choosing the classifier 
-
 
 ##classifying on train set
 
@@ -91,11 +78,11 @@ model.fit(np.array(X_train),np.array(y_train),
 
 ## testing on test
 
-#y_predicted = model.predict(X_test)
+y_predicted = model.predict(np.array(X_test))
 
 ##results 
 
-#print(accuracy_score(y_test, y_predicted))
+print(accuracy_score(y_test, y_predicted))
 
 
 #PREDICTION
